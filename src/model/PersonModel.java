6243bb97655071b92b14e068eb844ad80b95ad4b
mariaBarczyk <barczyk.maria92@gmail.com>
2018-02-22 10:13:49 +0100

    Create new PersonModel class

---
+++ src/model/PersonModel.java
@@ -0,0 +1,16 @@
+package model;
+
+public class PersonModel {
+
+    private int id;
+    private String name;
+    private String city;
+    private String country;
+
+    public PersonModel(int id, String name, String city, String country) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.country = country;
+    }
+}
