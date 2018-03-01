50a21830282d528d9f32f9363f6c43a081ac2c3b
mariaBarczyk <barczyk.maria92@gmail.com>
2018-03-01 21:06:38 +0100

    Add displaying collection method in RootView

--- src/view/RootView.java
+++ src/view/RootView.java
@@ -1,8 +1,19 @@
 package view;
 
+
+import java.util.List;
+
 public class RootView {
 
     public void displayMessage(String text) {
         System.out.println(text);
+    }
+
+    public <T> void  displayCollection(List<T> collection) {
+        for (T element: collection) {
+            System.out.println(element);
+        }
+
+
     }
 }
