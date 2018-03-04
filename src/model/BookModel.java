998241451d93c1dc87fbb7b13a44928699ec37f0
mariaBarczyk <barczyk.maria92@gmail.com>
2018-03-04 13:09:26 +0100

    Add getters in BookModel and change book type to object

--- src/model/BookModel.java
+++ src/model/BookModel.java
@@ -8,15 +8,43 @@ public class BookModel {
     private PersonModel publisher;
     private int publicationYear;
     private double price;
-    private String typeName;
+    private BookType type;
 
-    public BookModel(double ISBN, PersonModel author, String title, PersonModel publisher, int publicationYear, double price, String typeName) {
+    public BookModel(double ISBN, PersonModel author, String title, PersonModel publisher, int publicationYear, double price, BookType type) {
         this.ISBN = ISBN;
         this.author = author;
         this.title = title;
         this.publisher = publisher;
         this.publicationYear = publicationYear;
         this.price = price;
-        this.typeName = typeName;
+        this.type = type;
+    }
+
+    public double getISBN() {
+        return ISBN;
+    }
+
+    public PersonModel getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public PersonModel getPublisher() {
+        return publisher;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public BookType getType() {
+        return type;
     }
 }
