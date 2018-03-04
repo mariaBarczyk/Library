1527c093717578fc26f36c3cac806659a23cb1f1
mariaBarczyk <barczyk.maria92@gmail.com>
2018-03-04 20:45:04 +0100

    Create methods to edit book data in LibraryController

--- src/controller/LibraryController.java
+++ src/controller/LibraryController.java
@@ -35,7 +35,7 @@ public class LibraryController {
         PublisherDao publisherDao = new PublisherDao();
         List<PublisherModel> publishersCollection = publisherDao.getAllPublishers();
         rootView.displayListCollection(publishersCollection);
-        int authorId = inputController.getIntInput("Select author id: ");
+        String authorId = inputController.getStringInput("Select author id: ");
         return publisherDao.findPublisherById(authorId);
     }
 
@@ -60,8 +60,66 @@ public class LibraryController {
         BookModel newBook = bookDao.createBookObject(ISBN, author, title, publisher, publicationYear, price, type);
         bookDao.saveNewBook(newBook);
 
+    }
 
+    private BookModel selectBook() {
+        BookDao bookDao = new BookDao();
+        List<BookModel> allBooks = bookDao.getAllBooks();
+        rootView.displayListCollection(allBooks);
+        double ISBN = inputController.getDoubleInput("Enter ISBN: ");
+        BookModel matchedBook = null;
+        for (BookModel book : allBooks) {
+            if (book.getISBN() == ISBN) {
+                matchedBook = book;
+            }
+        }
+        return matchedBook;
+    }
 
-
+    public void editBook() {
+        BookModel bookToEdit = selectBook();
+        Double savedISBN = bookToEdit.getISBN();
+        int userChoice = 0;
+        while (userChoice != 8) {
+            rootView.displayEditBookMenu();
+            userChoice = inputController.getIntInput("SELECT AN OPTION: ");
+            switch (userChoice) {
+                case 1:
+                    String newTitle = inputController.getStringInput("Enter new title: ");
+                    bookToEdit.setTitle(newTitle);
+                    break;
+                case 2:
+                    AuthorModel newAuthor = selectAuthor();
+                    bookToEdit.setAuthor(newAuthor);
+                    break;
+                case 3:
+                    PublisherModel newPublisher = selectPublisher();
+                    bookToEdit.setPublisher(newPublisher);
+                    break;
+                case 4:
+                    int newPublicationYear = inputController.getIntInput("Enter publication year");
+                    bookToEdit.setPublicationYear(newPublicationYear);
+                    break;
+                case 5:
+                    double newISBN = inputController.getDoubleInput("Enter ISBN: ");
+                    bookToEdit.setISBN(newISBN);
+                    break;
+                case 6:
+                    BookType newType = selectType();
+                    bookToEdit.setType(newType);
+                    break;
+                case 7:
+                    double newPrice = inputController.getDoubleInput("Enter price: ");
+                    bookToEdit.setPrice(newPrice);
+                    break;
+                case 8:
+                    BookDao bookDao = new BookDao();
+                    bookDao.updateBookData(bookToEdit, savedISBN);
+                    rootView.displayMessage("Changes have benn saved.");
+                    break;
+                default:
+                    rootView.displayMessage("Wrong number");
+            }
+        }
     }
 }
