2c02bc579d40814d975fd64a591a5dfee84dbcfa
mariaBarczyk <barczyk.maria92@gmail.com>
2018-03-04 21:01:28 +0100

    Add displaying main and edit book menu in RootView

--- src/view/RootView.java
+++ src/view/RootView.java
@@ -23,6 +23,30 @@ public class RootView {
             System.out.println(type.getId() + ". " + type.getTypeName());
         }
     }
+
+    public void displayMainMenu() {
+        System.out.println("What would you like to do?\n" +
+                "1. Add new book\n" +
+                "2. Edit book data\n" +
+                "3. Search book\n" +
+                "4. Display books\n" +
+                "5. Display books of author\n" +
+                "5. Delete book\n" +
+                "6. Exit\n");
+    }
+
+    public void displayEditBookMenu() {
+        System.out.println("What would you like to edit?\n" +
+                "1. Title\n" +
+                "2. Author\n" +
+                "3. Publisher\n" +
+                "4. Publication year\n" +
+                "5. ISBN\n" +
+                "6. type\n" +
+                "7. price\n" +
+                "8. exit\n");
+    }
+
 }
 
 
